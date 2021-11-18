04e50ca962c35f6bffa7ff130f7a0bfe7ca6882f
Shawn Hartke <shawn.hartke@live.com>
2021-11-17 20:41:15 -0700

    Adding Next-Auth with github

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-   
+
 import * as React from 'react';
 import PropTypes from 'prop-types';
 import Head from 'next/head';
@@ -6,6 +6,7 @@ import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import { CacheProvider } from '@emotion/react';
 import theme from '../styles/mainTheme/theme';
+import { SessionProvider } from "next-auth/react"
 import createEmotionCache from '../infra/createEmotionCache';
 import '../styles/globals.css'
 
@@ -14,20 +15,22 @@ import '../styles/globals.css'
 const clientSideEmotionCache = createEmotionCache();
 
 export default function MyApp(props: any) {
-  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+  const { Component, emotionCache = clientSideEmotionCache, session, pageProps } = props;
 
   return (
-    <CacheProvider value={emotionCache}>
-      <Head>
-        <title>My page</title>
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
-      </Head>
-      <ThemeProvider theme={theme}>
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-        <CssBaseline />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </CacheProvider>
+    <SessionProvider session={session}>
+      <CacheProvider value={emotionCache}>
+        <Head>
+          <title>My page</title>
+          <meta name="viewport" content="initial-scale=1, width=device-width" />
+        </Head>
+        <ThemeProvider theme={theme}>
+          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+          <CssBaseline />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </CacheProvider>
+    </SessionProvider>
   );
 }
 
